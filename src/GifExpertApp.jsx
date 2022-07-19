6327a7d5db39e96637ad5e17779edca8d906455c
César León Schaus <cschaus@agropago.com>
2022-07-19 09:16:07 -0300

    [add] GifExpertApp structure by comments

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export const GifExpertApp = () => {
   return (
     <>
+      {/* title */}
       <h1>GifExpertApp</h1>
+
+      {/* input */}
+
+      {/* gif list */}
+        {/* gif item */}
+
     </>
   )
 }
