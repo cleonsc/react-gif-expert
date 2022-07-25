14688cf54247cd2a48f1fbdb6d82c664e72810fa
César León Schaus <cschaus@agropago.com>
2022-07-25 15:08:11 -0300

    [clean] logs and unnecesary code in gifGrid and getGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { getGifs } from '../helpers/getGifts';
 
 export const GifGrid = ({ category }) => {
-
-  const [counter, setCounter] = useState(10);
-
   useEffect(()=>{
     getGifs(category);
   }, []) //[] son las dependencias, si esta vacío este hook solo se ejecuta la primera vez q carga el componente
-  
 
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola Mundo</p>
-      <h5>{counter}</h5>
-      <button onClick={()=>setCounter(counter+1)}>+1</button>
     </>
   )
 }
