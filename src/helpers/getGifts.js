4b5976637538c274a5bf368a15ea7e4c5beaef64
César León Schaus <cschaus@agropago.com>
2022-07-25 17:57:53 -0300

    [edit] getGifs service

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -3,10 +3,10 @@ export const getGifs = async (category) => {
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
 
-  const gifts = data.map(img => ({
+  const gifs = data.map(img => ({
     id: img.id,
     title: img.title,
     url: img.images.downsized_medium.url
   }))
-  return gifts;
+  return gifs;
 }
