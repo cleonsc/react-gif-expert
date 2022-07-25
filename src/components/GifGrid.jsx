f3f150c35187df903eac9e2f3f335bb0b8b1dac7
César León Schaus <cschaus@agropago.com>
2022-07-25 17:58:27 -0300

    [edit] GifGrid component to show the images titles

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getGifts';
 
 export const GifGrid = ({ category }) => {
-  useEffect(()=>{
-    getGifs(category);
+
+  const [images, setImages] = useState([])
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages)
+  }
+
+  useEffect(() => {
+    //Opcion 1:
+    // getGifs(category)
+    //   .then(newImages => setImage(newImages));
+
+    //Opcion 2: envolver la funcion en otra funcion asyncrona
+    getImages(); //No hace falta esperarlo
+
   }, []) //[] son las dependencias, si esta vacío este hook solo se ejecuta la primera vez q carga el componente
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {
+          images.map(({id, title}) => (
+            <li key={id}>{title}</li>
+          ))
+        }
+      </ol>
     </>
   )
 }
