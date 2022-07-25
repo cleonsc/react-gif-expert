3e2869e02c24e3b869e3c8d4e079469f49e3a312
César León Schaus <cschaus@agropago.com>
2022-07-25 14:58:00 -0300

    [edit] limit of rows in getGifs

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=SsqnbACexRtYcnEjLZEd69J4hv1An40G&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=SsqnbACexRtYcnEjLZEd69J4hv1An40G&q=${category}&limit=10`;
   const resp = await fetch(url);
-  const {data =[]} = await resp.json();
+  const { data = [] } = await resp.json();
 
   const gifts = data.map(img => ({
     id: img.id,
