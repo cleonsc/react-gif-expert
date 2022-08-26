fac8de5a3acf33aa11e7b1e772b9658b040828f7
César León Schaus <cschaus@agropago.com>
2022-08-26 09:01:41 -0300

    [add] GifGrid test using a mock for useFetchGifs hook

---
+++ tests/components/GifGrid.test.jsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
+
+describe('Pruebas en <GifGrid/>', () => {
+  const category = 'One Punch';
+  test('should show the loading initially', () => {
+
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    })
+
+    render(<GifGrid category={category} />);
+    expect(screen.getByText('Cargando...'));
+    expect(screen.getByText(category));
+  });
+
+  test('should shows items when images are loaded through useFetchGifs', () => {
+    const gifs = [
+      {
+        id: 'abc',
+        title: 'Saitama',
+        url: 'https://localhost/saitama.jpg'
+      },
+      {
+        id: '123',
+        title: 'Goku',
+        url: 'https://localhost/goku.jpg'
+      }
+    ]
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: true
+    });
+
+    render(<GifGrid category={category} />);
+
+    expect(screen.getAllByRole('img').length).toBe(2);
+
+  });
+});
